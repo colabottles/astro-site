8034acb2f899077cb3d97661ce5b0341c62bfd02
Todd Libby <toddlibby@protonmail.com>
2025-01-12 02:00:46 -0700

    Made change with level one heading

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,15 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import UnoCSS from 'unocss/astro';
+
+import vue from '@astrojs/vue';
+
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    vue(),
+    UnoCSS(),
+  ],
+  adapter: netlify()
+});
