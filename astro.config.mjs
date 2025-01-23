39a2d42cf87b04819164707594ca126a18131728
Todd Libby <toddlibby@protonmail.com>
2025-01-23 10:56:44 -0700

    Added Header, Footer, Navigation components. Fixed styling

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,11 @@
 import { defineConfig } from 'astro/config';
-import UnoCSS from 'unocss/astro';
-
 import vue from '@astrojs/vue';
-
 import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     vue(),
-    UnoCSS(),
   ],
   adapter: netlify()
 });
